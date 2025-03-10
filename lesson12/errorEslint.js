a02774cd0071e93ed65ae9e7038e0af2a1e5e003
DanielKuka <114743521+DanielKuka@users.noreply.github.com>
2025-03-10 13:01:31 +0200

    Prettier and ESlint check

---
+++ lesson12/errorEslint.js
@@ -0,0 +1,12 @@
+function greet(name) {
+    if (name == 'Danylo') {
+      console.log('Hello, Danylo!');
+    } else {
+      console.log('Hello, stranger!');
+    }
+  
+    var greeting = 'Good morning';
+    console.log(greeting);
+  }
+  
+  greet('Alice');
--- lesson12/task1.js
+++ lesson12/task1.js
@@ -1,4 +1,42 @@
-console.log('Наче вдалось налаштувати')
+// _________________________Завдання 4: Обчислення площі та об'єму___________________________
 
+// __________Завдання 4.1__________
+// π - число “пі”. Ви можете використати Math.PI у вашому дз для вираження цього числа
+// Створіть змінну radius і присвойте їй числове значення радіуса кола.
+// Обчисліть площу кола за формулою π * radius^2 і виведіть результат.
 
-console.log('test')
+let radiusOfCircle = 4
+let circleArea = Math.PI * radiusOfCircle ** 2
+
+console.log('Площа кола = ', circleArea.toFixed(2))
+// console.log(typeof circleArea);
+
+// __________Завдання 4.2__________
+
+// Створіть змінну length і присвойте їй числове значення довжини прямокутника.
+// Створіть змінну width і присвойте їй числове значення ширини прямокутника.
+// Обчисліть площу прямокутника за формулою length * width і виведіть результат.
+
+let lengthOfRectangle = 6
+let widthOfRectangle = 3
+let rectanlgeArea = lengthOfRectangle * widthOfRectangle
+
+console.log('Площа прямокутника = ', rectanlgeArea.toFixed(2))
+// console.log(typeof rectanlgeArea);
+
+// __________Завдання 4.3__________
+
+// Створіть змінну radius і присвойте їй числове значення радіуса циліндра.
+// Створіть змінну height і присвойте їй числове значення висоти циліндра.
+// Обчисліть об'єм циліндра за формулою π * radius^2 * height і виведіть результат.
+// Округліть кожне отримане значення до 2 значень після крапки
+
+let radiusOfCylinder = 5
+let heightOfCylinder = 10
+
+let cylinderVolume = Math.PI * Math.pow(radiusOfCylinder, 2) * heightOfCylinder
+
+console.log('Обʼєм циліндра = ', cylinderVolume.toFixed(2))
+// console.log(typeof cylinderVolume);
+
+console.log('Hello World')
